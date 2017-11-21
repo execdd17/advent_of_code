a56ea56dcc691d8f585ecaccd933266fa76bfcc0
Alexander Vanadio <execdd17@gmail.com>
2017-11-20 22:47:36 -0500

    restructured for clarity

---
+++ day2/ComplexKeyPad.py
@@ -0,0 +1,12 @@
+class ComplexKeyPad(object):
+    """
+    #############
+    #     1     #
+    #   2 3 4   #
+    # 5 6 7 8 9 #
+    #   A B C   #
+    #     D     #
+    #############
+
+    taken from: http://adventofcode.com/2016/day/2
+    """
---
+++ day2/Driver.py
@@ -0,0 +1,26 @@
+from KeyPad import KeyPad
+
+if __name__ == "__main__":
+    keypad = KeyPad()
+    keypad.add_all_transitions()
+
+    input_lines = """ULL
+        RRDDD
+        LURDL
+        UUUUD""".replace(" ", "").split("\n")
+
+    # the correct answer is 78985
+    file = open('day2_input.txt', 'r')
+
+    for input_line in file.readlines():
+        for direction in input_line:
+            if direction == 'U':
+                keypad.push_up()
+            elif direction == 'D':
+                keypad.push_down()
+            elif direction == 'L':
+                keypad.push_left()
+            elif direction == 'R':
+                keypad.push_right()
+
+        print keypad.state
