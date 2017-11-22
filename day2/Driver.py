d8f47d451c4be4a549c364b899ea264d26a7d121
Alexander Vanadio <execdd17@gmail.com>
2017-11-22 00:14:23 -0500

    streamlined driver

--- day2/Driver.py
+++ day2/Driver.py
@@ -1,11 +1,14 @@
 from KeyPad import KeyPad
+from ComplexKeyPad import ComplexKeyPad
+import sys
 
-if __name__ == "__main__":
-    keypad = KeyPad()
-    keypad.add_all_transitions()
 
-    # the correct answer is 78985
-    file = open('day2_input.txt', 'r')
+def usage():
+    print "Expected either a '1' or '2' for part one or two, respectively"
+
+
+def compute_password(keypad, file, get_key):
+    password = []
 
     for input_line in file.readlines():
         for direction in input_line:
@@ -17,5 +20,30 @@ if __name__ == "__main__":
                 keypad.push_left()
             elif direction == 'R':
                 keypad.push_right()
+        password.append(get_key())
 
-        print keypad.state
+    print password
+
+
+def main():
+    if len(sys.argv) != 2:
+        usage()
+
+    # the correct answer is 78985 for part one
+    # the correct answer is 57DD8 for part two
+    file = open('day2_input.txt', 'r')
+
+    if sys.argv[1] == '1':
+        keypad = KeyPad()
+        keypad.add_all_transitions()
+        compute_password(keypad, file, lambda: keypad.state)
+    elif sys.argv[1] == '2':
+        keypad = ComplexKeyPad()
+        compute_password(keypad, file, lambda: keypad.current_cell.key)
+    else:
+        usage()
+
+
+if __name__ == "__main__":
+    main()
+
