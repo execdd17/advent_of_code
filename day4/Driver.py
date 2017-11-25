c3f0739cad69dfb99c1541c6de9cbae315d8647b
Alexander Vanadio <execdd17@gmail.com>
2017-11-25 00:31:24 -0500

    removed useless code

--- day4/Driver.py
+++ day4/Driver.py
@@ -1,16 +1,10 @@
 from Room import Room
 
 
-def convert_line_to_num_list(input_line):
-        sides = input_line.strip().split(" ")
-        intermediate_sides = list(filter(lambda x: x != '', sides))
-        return list(map(lambda x: int(x), intermediate_sides))
-
-
 def main():
 
     # the correct answer is 185371 for part one
-    # the correct answer is ????? for part two
+    # the correct answer is 984 for part two
     # file = open('puzzle_input.txt', 'r')
     file = open('puzzle_input.txt', 'r')
     lines = file.readlines()
